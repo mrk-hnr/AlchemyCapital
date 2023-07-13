bb7569f4ff2c4784431f4627d42b3848f3d8a140
mrk-hnr <mrk.hnr@hotmail.com>
2023-07-13 18:38:14 +0800

    Committing completed site

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
-const elOne = document.querySelector('.hoverOne');
-const elTwo = document.querySelector('.hoverTwo');
-const elThree = document.querySelector('.hoverThree');
-const elFour = document.querySelector('.hoverFour');
+const mainMenu = document.querySelector('.mainMenu')
+const closeMenu = document.querySelector('.closeMenu')
+const openMenu = document.querySelector('.openMenu')
 
-const hiddenDivOne = document.querySelector('.hideMeOne');
-const hiddenDivTwo = document.querySelector('.hideMeTwo');
-const hiddenDivThree = document.querySelector('.hideMeThree');
-const hiddenDivFour = document.querySelector('.hideMeFour');
 
-// ✅ Show hidden DIV on hover
-// HIDES CONTENT 1
-elOne.addEventListener('mouseover', function handleMouseOver() {
-    hiddenDivOne.style.display = 'block'; })
 
-elOne.addEventListener('mouseout', function handleMouseOut() {
-    hiddenDivOne.style.display = 'none'; })
+openMenu.addEventListener('click', show) 
+closeMenu.addEventListener('click', close) 
 
-// HIDES CONTENT 2
-elTwo.addEventListener('mouseover', function handleMouseOver() {
-    hiddenDivTwo.style.display = 'block'; })
+function show() {
+    mainMenu.style.display = 'flex'
+    mainMenu.style.top = '0'
+}
 
-elTwo.addEventListener('mouseout', function handleMouseOut() {
-    hiddenDivTwo.style.display = 'none'; })
-        
-// HIDES CONTENT 3
-elThree.addEventListener('mouseover', function handleMouseOver() {
-    hiddenDivThree.style.display = 'block'; })
-
-elThree.addEventListener('mouseout', function handleMouseOut() {
-    hiddenDivThree.style.display = 'none'; })
-        
-// HIDES CONTENT 4
-elFour.addEventListener('mouseover', function handleMouseOver() {
-    hiddenDivFour.style.display = 'block'; })
-
-elFour.addEventListener('mouseout', function handleMouseOut() {
-    hiddenDivFour.style.display = 'none'; })
-
-    
+function close() {
+    mainMenu.style.top = '-100%'   
+}
